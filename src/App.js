8ebfa102627c3c369db322398dad1149c7b9f1f5
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-09-12 12:22:34 +0900

    styled component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponent";
 
 function App() {
   return (
     <div className="App">
-      <CSSModule />
+      <StyledComponent />
     </div>
   );
 }
